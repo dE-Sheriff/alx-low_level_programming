53f6829b5ecc346e0acfca4bf76e9300c02c458b
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-20 09:43:44 -0700

    Concluding c scripts for mandatory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("\nSize of int is %d bytes", sizeof(int));
+	printf("\nSize of float is %d bytes", sizeof(float));
+	printf("\nSize of short int is %d bytes", sizeof(short int));
+	printf("\nSize of signed int is %d bytes", sizeof(signed int));
+	printf("\nSize of unsigned int is %d bytes", sizeof(unsigned int));
+	printf("\nSize of long int is %d bytes", sizeof(long int));
+	printf("\nSize of long long int is %d bytes", sizeof(long long int));
+	printf("\nSize of double is %d bytes", sizeof(double));
+	printf("\nSize of long double is %d bytes", sizeof(long double));
+	printf("\nSize of signed short int is %d bytes", sizeof(signed short int));
+	printf("\nSize of signed long int is %d bytes", sizeof(signed long int));
+	printf("\nSize of unsigned short int is %d bytes", sizeof(unsigned short int));
+	printf("\nSize of unsigned long int is %d bytes", sizeof(unsigned long int));
+	printf("\nSize of unsigned long long int is %d bytes", sizeof(unsigned long long int));
+	printf("\nSize of char is %d bytes", sizeof(char));
+	printf("\nSize of signed char is %d bytes", sizeof(signed char));
+	printf("\nSize of unsigned char is %d bytes", sizeof(unsigned char));
+   	return (0);
+}
