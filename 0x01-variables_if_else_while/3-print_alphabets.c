6205f0bf2b04b5cdbc45d4c842c2ed7e856e8d94
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-22 15:08:55 -0700

    edit on 2 and 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,9 +5,11 @@ int main(void)
         char ch = 'a';
         while (ch <= 'z')
                 putchar(ch);
+		ch++
 
-        char ch = 'A'
-        while (ch <= 'Z')
-                putchar(ch);
+        char CH = 'A'
+        while (CH <= 'Z')
+                putchar(CH);
+		CH++
         return (0);
 }
