2c08cd52ddf272d48a3f844a176246d7630e7860
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-23 15:45:10 -0700

    script 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers of base ten.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0
+	/* using whilev loop */
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
