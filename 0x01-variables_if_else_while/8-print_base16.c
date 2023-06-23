e23d8b810d0b0abda036c78313beca3d3e7c330b
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-23 16:21:59 -0700

    script 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all lowercase letters \
+then uppercase letter followed by new line.
+ * You can only use `putchar` to print to the console.
+ * You can only use `putchar` up to three times.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	int num = 0;
+	/* using while loop */
+
+	while (num <= 9)
+	{
+		putchar('num');
+		num++;
+	}
+	/* Another while loop */
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
