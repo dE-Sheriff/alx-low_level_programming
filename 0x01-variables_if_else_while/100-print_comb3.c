b85b870738921938552b1520e52ef872b23d18f2
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-24 00:28:07 -0700

    edit on 100, remove white space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
- * main  prints all possible different 
+ * main  prints all possible different\
  * combinations of two digits\
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	/* using For loops and if conditions loop */
+	
 	int num1, num2;
-	 /* using For loops and if conditions loop */
-	 {
-		for (num1 = 0;num2 <= 9;num1++)
+	{
+		for (num1 = 0; num2 <= 9; num1++)
 		{
-			for (num2 = 0;num2 <= 9;num2++)
+			for (num2 = 0; num2 <= 9; num2++)
 			{
 				if ((num1 != num2) && (num1 < num2))
 				{
