83bd80309cbfe636e23316966ca2e3bc22d4e1f4
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-24 23:02:51 -0700

    first script on nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* print _putchar*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
