f8af970bec0d69b672ad31c2877be157dfd5b0e2
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-25 23:50:21 -0700

    edit on 2 again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,18 @@
 */
 
 void print_alphabet_x10(void)
+/* using for loop */
 {
-	/* using for loop */
 	int n;
 	for (n = 1; n <= 10; n++)
 	{
 		char ch = 'a';
+/* using while loop */
 		while (ch <= 'z')
 		{
 			_putchar(ch);
+			_putchar('\n')
 			ch++;
 		}
 	}
-	_putchar('\n');
 }
