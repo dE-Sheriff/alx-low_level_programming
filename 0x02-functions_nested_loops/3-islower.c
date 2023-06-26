ab3643b8d77376a0614d645225d249f2c5a67374
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-26 00:21:47 -0700

    edit on 3 c comment

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
 * _islower - print alphabets using _putchar
-* Return: 0
+* @c:character to check if it islowercase.
+* Return: 0 if true return: 1 if false
 */
 
 int _islower(int c)
+/* using if condition */
 {
 	if ((c >= 'a') && (c <= 'z'))
 	{
