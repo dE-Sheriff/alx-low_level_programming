5513b5a243fa5399d2e03c49128e81b6ede8a7d9
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-26 22:50:21 -0700

    edit on 5 fix return statement

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,17 +6,18 @@
 */
 
 int print_sign(int n)
-/* using if condition */
+/* n auto provided by header file
+ * using if condition */
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar ('0');
-		return(0);
+		return (0);
 	}
 	else if (n < 0)
 	{
