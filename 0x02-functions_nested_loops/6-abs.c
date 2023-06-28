1d90d190ed499ad3471e81198928957eabbd6ad6
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-28 09:24:45 -0700

    edit on 6, abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_abs - print absolute value of character.
+* @a:character to check if it is lowercase.
+* Return: 0 if true return: 1 if false
+*/
+
+int _abs(int a)
+/* using if condition */
+{
+	/* n auto provided by header file*/
+	anum = _abs(a0)
+	{
+		_putchar('anum')
+	}
+	return (0);
+}
