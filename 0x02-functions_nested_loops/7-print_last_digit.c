10c58c22a13c2b93a6e295118ab6d4be741d9b89
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-28 11:35:07 -0700

    edit 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_last_digit - print alphabets using _putchar
+* @a: integer to be turned to absolute value.
+* Return: 0 if true return: 1 if false
+*/
+
+int print_last_digit(int a)
+/* using if condition */
+{
+	/* n auto provided by header file*/
+	int last_dig;
+
+	last_dig = a % 10;
+	if (a < 0)
+	{
+		last_dig = last_dig *(-1);
+		_printf('last_dig')
+	{
+}
