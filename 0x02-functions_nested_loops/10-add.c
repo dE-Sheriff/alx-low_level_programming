9641ea49054fcc736c93497bceafec2bd357f5d8
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-29 02:04:25 -0700

    edit on 10, insert arguments description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 #include "stdio.h"
 /**
 * add - print alphabets using _putchar
+* @x: First argnument to pass
+* @y: secon of the argument to be passed
 * Return: 0 if true return: 1 if false
 */
 
