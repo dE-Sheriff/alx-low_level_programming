564cae00a8bcb001bbb1fdc2ebb337150f012b44
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-29 02:43:02 -0700

    edit on script 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,28 @@
 #include "main.h"
 #include "stdio.h"
 /**
-* add - print alphabets using _putchar
+* print_to_98 - print alphabets using _putchar
+* @n: iput to be ranged
 * Return: 0 if true return: 1 if false
 */
 
-int add(int, int)
+void print_to_98(int n)
 /*sum of two integers */
 {
 	/* no special function */
-	int x,y;
-
-	sum = a + b;
-	return (sum);
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n)
+		}
+	}
+	printf("%d\n", n);
 }
