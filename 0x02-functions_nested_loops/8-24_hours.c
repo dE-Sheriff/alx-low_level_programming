72b81696ca90dc17d7c1bf983e0a9a254f4a50d9
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-28 20:40:38 -0700

    start with script 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* jack_bauer - print alphabets using _putchar
+* Return: 0 if true return: 1 if false
+*/
+
+void jack_bauer(void)
+/* printin every minut in 24 hours */
+{
+	/*using for loops */
+	int w, x, y, z;
+
+	for (w = 0; w <= 2; w++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = 0; y <= 5; y++)
+			{
+				for (z = 0; z <= 9; z++)
+				{
+					if (w == 2 && x > 3)
+					{
+						break;
+					}
+					_putchar(w);
+					_putchar(x);
+					_putchar(':');
+					_putchar(y);
+					_putchar(z);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
