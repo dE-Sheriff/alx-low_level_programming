bfb70914cf484cf12528a8c70c25ea8ed9d76366
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-30 01:10:53 -0700

    edit on 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* main - print alphabets using _putchar
+* @n: iput to be ranged
+* Return: 0 if true return: 1 if false
+*/
+
+int main(void)
+/*sum of two integers */
+{
+	/* no special function */
+	int x, y;
+	for (x =1; x < 1024; x++)
+	{
+		if((x %3) == 0 || (x % 5) == 0)
+			y += x;
+	}
+	printf("%d\n", y);
+	return (0);
+}
