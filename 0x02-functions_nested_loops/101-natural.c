120332fccc6195181766a51cddbac21baacf635a
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2023-06-30 01:15:32 -0700

    edit on 101 betty

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,6 @@
 #include "stdio.h"
 /**
 * main - print alphabets using _putchar
-* @n: iput to be ranged
 * Return: 0 if true return: 1 if false
 */
 
@@ -11,11 +10,13 @@ int main(void)
 {
 	/* no special function */
 	int x, y;
-	for (x =1; x < 1024; x++)
+
+	for (x = 1; x < 1024; x++)
 	{
-		if((x %3) == 0 || (x % 5) == 0)
+		if ((x %3 ) == 0 || (x % 5) == 0)
 			y += x;
 	}
 	printf("%d\n", y);
 	return (0);
+
 }
