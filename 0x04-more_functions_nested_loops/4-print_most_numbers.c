07fbc03a6447810e484e42d52f3a7470d37af2af
master <sheriffsarafa@gmail.com>
2023-09-15 06:37:25 -0700

    edit 2 on print most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,16 +8,12 @@
 int print_most_numbers(void) 
 {
 	char c;
-	for (c = 0; c <= 9;)
+	for (c = 0; c <= 9; c++)
 	{
-		if ((c == 2) && (c == 4))
-		{
-			c++;
-		}
-		else
+		if ((c != 2) || (c != 4))
 		{
 			_putchar('c');
-			c++;
 		}
+		_putchar('\n')
 	}
 }
