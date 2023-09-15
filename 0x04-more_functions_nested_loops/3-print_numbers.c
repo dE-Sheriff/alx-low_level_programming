43a06fa0d802f0dd4289335eacf8381efd4c8511
master <sheriffsarafa@gmail.com>
2023-09-15 02:54:04 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * return - Viod
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a < '10', a++)
+	{
+		_putchar(a);
+		_putchar(\n);
+	}
+}
+
