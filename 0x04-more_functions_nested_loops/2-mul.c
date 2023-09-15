1fb27921998236646edb2d33fcf0e99930455bfd
master <sheriffsarafa@gmail.com>
2023-09-15 01:13:12 -0700

    edit 05 on mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,12 @@
  * mul - num to multiply
  * @a: first integer
  * @b: second integer
- * @c: product of both
- * Return: c
+ * c: product of both
+ * Return: a * b
  */
 
 int mul(int a, int b)
 {
-	int c = a*b;
+	int c = a * b;
 	return (c);
 }
