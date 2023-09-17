87b90be3f2847c69407ab1c80ccfdbe3d705e549
master <sheriffsarafa@gmail.com>
2023-09-16 23:21:42 -0700

    edit on 5-more nums 06

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,11 @@ void more_numbers(void)
 	for (a = 0; a < 10; a++)
 	{
 		for (c = 0; c <= 14; c++)
+		{
 			if (c >= 10)
 				_putchar('1');
 			_putchar(c % 10 + '0');
-	}
+		}
 	_putchar('\n');
+	}
 }
