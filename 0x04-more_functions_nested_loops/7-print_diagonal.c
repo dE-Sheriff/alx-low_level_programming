ecbee88f52055b502d86aa21771d7a4a00d83acd
master <sheriffsarafa@gmail.com>
2023-09-17 04:48:49 -0700

    add on 07 o1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line
+ * @n: number of times _ is printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int a;
+		int b;
+
+		for (a = 1; a <= n; a++)
+		{
+			for(b = 0; b < n; b++)
+			{
+				_putchar('_');
+			}
+			if (b == a)
+			{
+				_putchar(\)
+			}
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
