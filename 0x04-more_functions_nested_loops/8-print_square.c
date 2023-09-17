d534cd333671cd38eb005a6de371d2ed343a2d6c
master <sheriffsarafa@gmail.com>
2023-09-17 12:45:34 -0700

    add 08 print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: number of times \ is printed
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int a = 0;
+		int b;
+
+		while (a < size)
+		{
+			b = 0;
+			while (b < size)
+			{
+				_putchar('#');
+				b++;
+			}
+			++a;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
