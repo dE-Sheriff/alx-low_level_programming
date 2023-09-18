6eae26a5cc759151034f5f24893e780d8f1fc909
master <sheriffsarafa@gmail.com>
2023-09-18 02:18:38 -0700

    edit on 9 01

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,29 @@ void main(void)
 {
 	int a;
 
-	for (a = 0; a <= 100; a++)
+	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 5 == 0) && (a % 3 != 0))
-		{
-			printf("Buzz");
-
-		}
-		else if ((a % 3 == 0) && (a % 5 != 0))
+		if ((a % 3 == 0) && (a % 5 != 0))
 		{
 			printf("Fizz");
 		}
-		else if (a == 1)
+		else if ((a % 5 == 0) && (a % 3 != 0))
 		{
-			printf("%d", a);
+			printf("Buzz");
 		}
 		else if ((a % 3 == 0) && (a % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
+		else if (a == 1)
+		{
+			printf("%d", a);
+		}
 		else
 		{
 			printf("%d", a);
 		}
+		printf(" ");
 	}
 	printf("\n");
 
