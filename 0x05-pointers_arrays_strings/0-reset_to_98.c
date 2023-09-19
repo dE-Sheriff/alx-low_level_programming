9a7d512c72d2f6b93985e2a484cb95452255a96e
master <sheriffsarafa@gmail.com>
2023-09-19 01:02:01 -0700

    add on reset to 98 03

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,10 @@
 /**
  * reset_to_98 - updates the value it points to to 98
  * @n: the int parameter
- * Return: (0)
+ * Return: n
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	_putchar('\n');
 }
