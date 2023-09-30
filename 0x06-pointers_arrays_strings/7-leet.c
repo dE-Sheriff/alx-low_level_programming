af7dab70274fb6c53f9e80b7b55ba49df85594d5
master <sheriffsarafa@gmail.com>
2023-09-30 09:27:38 -0700

    add on 07 mP&a 01

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337 
+ * @n: second string
+ * Return: char
+ */
+
+char *leet(char *n)
+{
+	int a;
+
+	a = 0;
+	while (n[a] != '\0')
+	{
+		if ((n[a] == 'a') || (n[a] == 'A'))
+		{
+			n[a] = '4';
+		}
+		else if ((n[a] == 'e') || (n[a] == 'E'))
+		{
+			 n[a] = '3';
+		}
+		else if ((n[a] == 'o') || (n[a] == 'O'))
+		{
+			n[a] = '0';
+		}
+		else if ((n[a] == 't') || (n[a] == 'T'))
+		{
+			n[a] = '7';
+		}
+		else if ((n[a] == 'l') || (n[a] == 'L'))
+		{
+			n[a] = '1';
+		}
+		a++
+	}
+	return (n);
+}
