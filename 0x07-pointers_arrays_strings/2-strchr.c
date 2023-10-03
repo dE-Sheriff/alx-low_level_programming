176873bbc8ee276dea88eb9af7440c66e85902da
master <sheriffsarafa@gmail.com>
2023-10-02 19:44:35 -0700

    edit on 02 EMP&A - 04

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != c)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		++i;
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	if (s[i] >= '\0')
-		return (0);
-	return (&s[i]);
+	return (0);
 }
