9641b3af52b8c4ebb3f439eb95a20802b88867a7
master <sheriffsarafa@gmail.com>
2023-10-09 00:25:25 -0700

    add on 01 moreMALLOC 00

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,12 +4,38 @@
 
 /**
  * string_nconcat - a function that concatenates two strings
- * @b: the amount of memory
- * Return: void
+ * @n: the amount of memory
+ * @s1: 1st string
+ * @s2: second string
+ * Return: char
  */
 
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int s1x = 0, s2x = 0, a, b;
+	char *s;
 
+	while (s1[s1x] != '\0')
+	{
+		s1x++;
+	}
+	while (s2[s2x] != '\0' && s2x <= n)
+	{
+		s2x++;
+	}
+	if (n >= s2x)
+		n = s2x;
+	s = malloc(sizeof(char) * (s1x + s2x + 1));
+
+	if (!s)
+		return (NULL);
+	for (a = 0; a <= s1x; a++)
+		s[a] = s1[a]
+	for (b = 0; a <= (s1x + n); b++)
+	{
+		a++
+		s[a] = s2[b]
+	}
+	return (s);
 }
