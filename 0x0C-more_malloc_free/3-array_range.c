3cdb53ba87ea44db896666415a23a92d5b65d181
master <sheriffsarafa@gmail.com>
2023-10-10 01:14:59 -0700

    add on 03 more malloc 07

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	j = max - min + 1
+	j = max - min + 1;
 	arr = malloc(sizeof(int) * j); 
 	if (arr == NULL)
 		return (NULL);
