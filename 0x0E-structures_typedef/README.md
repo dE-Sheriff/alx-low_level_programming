a6f9970f2e7958c4a1880357a08b1eb0bb4d3be8
master <sheriffsarafa@gmail.com>
2023-10-11 02:47:26 -0700

    add on oo struct 00

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+6 mandatory projects
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - a new type
+ * @name: - name
+ * @age: age -nt
+ * @owner: name owner
+ *
+ * Description: Defines a new type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
