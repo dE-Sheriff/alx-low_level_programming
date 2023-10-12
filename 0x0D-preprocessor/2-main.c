b1176520d39d90fe022f9f7bfbf1e001647d8fa3
master <sheriffsarafa@gmail.com>
2023-10-11 23:22:06 -0700

    edit on preprocessor 01

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - a program that prints the name of the file
- * it was compiled from, followed by a new line 
+ * it was compiled from, followed by a new line
  *
  * Return: Always 0 (Success)
  */
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,7 @@
 #ifndef SUM_H
 #define SUM_H
 
-#define SUM(x, y) ((X) + (Y))
+#define SUM(x, y) (x) + (y))
 
 #endif
 
