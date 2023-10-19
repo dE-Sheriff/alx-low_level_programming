a5fa934bc0416c4660751534c9a6a21cad79847d
master <sheriffsarafa@gmail.com>
2023-10-19 14:50:46 -0700

    edit on 00 LinkedL 01

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0x12. C - Singly linked lists
+4 mandatory projects
+2 Advanced
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,12 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* LIST_H */
