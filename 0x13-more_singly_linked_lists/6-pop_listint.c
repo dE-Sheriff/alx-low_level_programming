5e9320d7a5fd761cc03fe000f6b2cf43aba240b2
Sheriff <sheriffsarafa@gmail.com>
2023-10-25 01:40:07 +0100

    add on 06 MLL 02

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *holder;
 	int x;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	x = (*head)->n;
 	holder = (*head)->next;
