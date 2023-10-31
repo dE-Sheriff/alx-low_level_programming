5b41176952007028a53cdab420ea93113a0db720
Sheriff <sheriffsarafa@gmail.com>
2023-10-31 04:04:53 +0100

    add on 01 i/o 02

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, count, num_wr;
-	char emp = '\0';
+	char *emp = "";
 
 	if (!filename)
 		return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <errno.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
