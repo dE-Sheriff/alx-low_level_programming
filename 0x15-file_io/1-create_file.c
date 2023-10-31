3e361355b0ae0123411a9b4479bf65e2abed1cb3
Sheriff <sheriffsarafa@gmail.com>
2023-10-31 03:44:06 +0100

    add on 01 i/o 02

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
-
-	while (text_content)
-		count++;
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
 	if (*text_content == '\0')
 		num_wr = write(STDOUT_FILENO, &emp, 1);
 	else
