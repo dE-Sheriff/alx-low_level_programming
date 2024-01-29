2d7bc4ebd0ec02b0d66b98d583fa015ea770108c
Sheriff Sarafa <sheriffsarafa@gmail.com>
2024-01-29 21:30:51 +0100

    add on DynLIb 00

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	char c;
+
+	while (*str != '\0')
+	{
+		c = *str++;
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src, including the
+ * terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: source
+ * @src: to
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for ( ; b < a ; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
